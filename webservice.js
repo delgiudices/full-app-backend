afa28912d0221d29bc77a0740b6786b95cf894d6
Luis Del Giudice <luis.dg19@gmail.com>
2015-03-31 16:56:28 -0400

    Added support for ?yesterday=true to show yesterday's posts

--- webservice.js
+++ webservice.js
@@ -8,12 +8,24 @@ module.exports = function(Event) {
 
         var today = new Date();
         today.setHours(today.getHours() - 6);
+
+        if (req.query.yesterday === 'true' )
+          today.setHours(today.getHours() - 24);
+
         var queryDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 8);
         var queryTime = queryDate.getTime() / 1000;
-        
-        
 
-        var query = Event.find({ instagram_date : { "$gte" : queryTime } }).limit(30);
+        if ( req.query.yesterday === 'true' )
+          today.setHours(today.getHours() + 24);
+        else
+          today.setHours(today.getHours() + 48);
+
+        var tillTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 8).getTime() / 1000;
+
+
+
+        // var query = Event.find({ instagram_date : { "$gte" : queryTime, "$lte" : new Date()} }).limit(30);
+        var query = Event.where('instagram_date').gte(queryTime).lte(tillTime).limit(30);
 
         if ( req.query.hot === 'true' )
             query.sort({ 'score' : -1, 'instagram_date' : -1 });
@@ -22,7 +34,7 @@ module.exports = function(Event) {
 
         query.exec(function(err, events) {
             res.json(events);
-        });    
+        });
     });
 
     var generateLikeFunction = function(amount) {
@@ -37,7 +49,7 @@ module.exports = function(Event) {
     }
 
     app.get('/posts/:id/up', generateLikeFunction(1));
-    app.get('/posts/:id/down', generateLikeFunction(-1)); 
+    app.get('/posts/:id/down', generateLikeFunction(-1));
 
     app.listen(1337);
 
